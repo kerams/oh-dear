fe16f3bfbe0845217c7295090c7f7a5f1cc0aad9
kerams <kerams@users.noreply.github.com>
2023-03-26 16:48:19 +0200

    Init

---
+++ Program.fs
@@ -0,0 +1,37 @@
+﻿module T
+
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Configs
+open System.Runtime.CompilerServices
+
+[<MemoryDiagnoser>]
+type Current () =
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let o = Some 10
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let vo = ValueSome 10
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let r = Result<int, string>.Ok 10
+
+    [<Benchmark>]
+    member _.Option() =
+        match o with
+        | Some x -> Some (x + 3)
+
+    [<Benchmark>]
+    member _.Voption() =
+        match vo with
+        | ValueSome x -> ValueSome (x + 3)
+
+    [<Benchmark>]
+    member _.Result() =
+        match r with
+        | Ok x -> Result<int, string>.Ok (x + 3)
+
+BenchmarkDotNet.Running.BenchmarkRunner.Run (
+    typeof<Current>.Assembly,
+    DefaultConfig.Instance.WithOption (ConfigOptions.JoinSummary, true))
+|> ignore
---
+++ README.md
@@ -0,0 +1,15 @@
+﻿``` ini
+
+BenchmarkDotNet=v0.13.5, OS=Windows 11 (10.0.22621.1413/22H2/2022Update/SunValley2)
+AMD Ryzen 9 7900, 1 CPU, 24 logical and 12 physical cores
+.NET SDK=8.0.100-preview.2.23157.25
+  [Host]     : .NET 7.0.3 (7.0.323.6910), X64 RyuJIT AVX2 DEBUG
+  DefaultJob : .NET 7.0.3 (7.0.323.6910), X64 RyuJIT AVX2
+
+
+```
+|  Method |      Mean |     Error |    StdDev |   Gen0 | Allocated |
+|-------- |----------:|----------:|----------:|-------:|----------:|
+|  Option | 2.1162 ns | 0.0626 ns | 0.0522 ns | 0.0014 |      24 B |
+| Voption | 3.0695 ns | 0.0059 ns | 0.0046 ns |      - |         - |
+|  Result | 0.2416 ns | 0.0052 ns | 0.0043 ns |      - |         - |
